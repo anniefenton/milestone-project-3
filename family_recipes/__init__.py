4a4dacbccd7d473f270c3527daa01e359deeb612
anniefenton <anniefentoncontact@gmail.com>
2022-08-10 14:45:00 +0000

    Update __init__.py file

--- family_recipes/__init__.py
+++ family_recipes/__init__.py
@@ -6,7 +6,11 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+if os.environ.get("DEVELOPMENT") == "TRUE":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 db = SQLAlchemy(app)
 
