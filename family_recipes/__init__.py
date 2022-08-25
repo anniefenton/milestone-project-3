104a8157403a22a32ab275b30d7630d41fc36a48
anniefenton <anniefentoncontact@gmail.com>
2022-08-25 12:01:11 +0000

    create navbar from materialize

--- family_recipes/__init__.py
+++ family_recipes/__init__.py
@@ -1,21 +1,15 @@
 import os
-import re
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env #noqa
+    import env  # noqa
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-
-if os.environ.get("DEVELOPMENT") == "TRUE":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
-else:
-    uri = os.environ.get("DATABASE_URL")
-    if uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = uri # heroku
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 
-from family_recipes import routes #noqa
+from family_recipes import routes  # noqa
+
