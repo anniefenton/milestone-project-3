0c60ebea848e334d5783acbf5e0a7de116fa31e0
anniefenton <anniefentoncontact@gmail.com>
2022-08-25 12:33:34 +0000

    initialize navbar on base.html and create footer on base.html

--- family_recipes/static/css/js/script.js
+++ family_recipes/static/css/js/script.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
   });
