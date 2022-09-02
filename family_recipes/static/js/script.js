7a376e4b9526db5f7a783d9cece17bed363fc932
anniefenton <anniefentoncontact@gmail.com>
2022-09-02 16:31:36 +0000

    add responsive hero image to home.html

--- family_recipes/static/js/script.js
+++ family_recipes/static/js/script.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   let sidenav = document.querySelectorAll('.sidenav');
   M.Sidenav.init(sidenav);
+
+  var elems = document.querySelectorAll('.materialboxed');
+  var instances = M.Materialbox.init(elems, options);
 });
