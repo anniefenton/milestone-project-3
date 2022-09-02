2714a6a6009d1fee4a119f33dd65ae369379c8a1
anniefenton <anniefentoncontact@gmail.com>
2022-09-02 19:48:19 +0000

    create templates for diet.html and add_diet.html

--- family_recipes/models.py
+++ family_recipes/models.py
@@ -7,8 +7,8 @@ class User(db.Model):
     first_name = db.Column(db.String(30), unique=False, nullable=False)
     last_name = db.Column(db.String(30), unique=False, nullable=False)
     username = db.Column(db.String(15), unique=True, nullable=False)
-    email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(30), unique=True, nullable=False)
+    password = db.Column(db.String(255), unique=True, nullable=False)
 
     def __repr__(self):
         # __repr__ to represent self as a string
--- family_recipes/routes.py
+++ family_recipes/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template
-from family_recipes import app, db
-from family_recipes.models import Diet, Recipe
+from flask import render_template, request, url_for, redirect, flash, session
 from werkzeug.security import generate_password_hash, check_password_hash
+from family_recipes import app, db
+from family_recipes.models import User, Diet, Recipe
+
 
 @app.route("/")
 def home():
@@ -13,6 +14,31 @@ def login():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+
+    if request.method == "POST":
+        existing_user = User.query.filter(
+            User.username == request.form.get("username").lower()).all()
+
+        if existing_user:
+            flash("This username already exists")
+            return redirect(url_for("register"))
+            
+        new_user = User(
+            username=request.form.get("username").lower(),
+            email=request.form.get("email").lower(),
+            password=generate_password_hash(request.form.get("password")),
+        )
+
+        test_pass = generate_password_hash(request.form.get("password"))
+        print(f"Pass: {test_pass}")
+        print(f"Length: {len(test_pass)}")
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash('Registration successful!')
+        return redirect(url_for('login'))
+
     return render_template("register.html")
 
 @app.route("/recipes")
